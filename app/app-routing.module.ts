e0a10738b514fc79f7bcc31f294fc70f125c7a00
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-06 05:01:15 +0200

    commit for create the route  in navbar for home , cart details and wish list details

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {BodyComponent} from './components/body/body.component';
+import {CartCounterDetailsComponent} from './components/cart-counter-details/cart-counter-details.component';
+import {WishListCounterDetailsComponent} from './components/wish-list-counter-details/wish-list-counter-details.component';
 const routes: Routes = [
-
+{path:'home' ,component:BodyComponent},
+{path:'cartlist' , component:CartCounterDetailsComponent},
+{path:'wishlist' , component:WishListCounterDetailsComponent}
 ];
 
 @NgModule({
