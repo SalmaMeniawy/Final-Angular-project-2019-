c6a0af380ca0f15210183202ffbf2810bc90cd11
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-06 18:53:20 +0200

    commit for write the mock data that related to products

---
+++ app/data/mock-products.ts
@@ -0,0 +1,67 @@
+import {Product} from 'src/app/models/product';
+export const products : Product[] = [
+    {
+        id : "HT-1000",
+         name:"Notebook Basic 15" , 
+         price:956,
+         description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+         img:"https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+    },
+    {
+        id: "HT-1022",
+        name: "Comfort Easy",
+        price: 1679,
+        description :"32 GB Digital Assistant with high-resolution color screen" ,
+        img : "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1022.jpg",
+    },
+    {
+        id:"HT-1023",
+        name: "Comfort Senior",
+        price:512,
+        description : "64 GB Digital Assistant with high-resolution color screen and synthesized voice output",
+        img:"https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1023.jpg",
+    },
+    {
+        id:"HT-1031",
+        name:"Ergo Screen E-II",
+        price:285,
+        description:"Optimum Hi-Resolution max. 1920 x 1200 @ 85Hz, Dot Pitch: 0.26mm",
+        img:"https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1031.jpg",
+    },
+    {
+        id:"HT-1042",
+        name: "Laser Allround",
+        price:349,
+        description:"Print up to 25 ppm letter and 24 ppm A4 color or monochrome, with Available first-page-out-time of less than 13 seconds for monochrome and less than 15 seconds for color",
+        img:"https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1042.jpg",
+    },
+    {
+        id:"HT-1050",
+        name:"Ultra Jet Super Color",
+        price:139,
+        description:"4800 dpi x 1200 dpi - up to 35 ppm (mono) / up to 34 ppm (color) - capacity: 250 sheets - Hi-Speed USB, Ethernet",
+        img:"https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1050.jpg",
+    },
+    {
+        id:"HT-1052",
+        name:"Ultra Jet Super Highspeed",
+        price: 170,
+        description:"4800 dpi x 1200 dpi - up to 35 ppm (mono) / up to 34 ppm (color) - capacity: 250 sheets - Hi-Speed USB2.0, Ethernet",
+        img:"https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1052.jpg",
+    },
+    {
+        id: "HT-1060",
+        name:"Cordless Mouse",
+        price:987,
+        description:"Cordless Optical USB Mice, Laptop, Color: Black, Plug&Play",
+        img:"https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1060.jpg",
+    },
+    {
+        id:"HT-1055",
+        name:"Multi Print",
+        price:99,
+        description:"1000 dpi x 1000 dpi - up to 16 ppm (mono) / up to 15 ppm (color)- capacity 80 sheets - scanner (216 x 297 mm, 1200dpi x 2400dpi)",
+        img:  "https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1055.jpg",
+    }
+    
+]
