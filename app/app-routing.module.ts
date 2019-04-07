88afdd23a160d47c765832b3bf9b3e2ea78a2bf1
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-07 23:02:40 +0200

    commit for create route for register form

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {BodyComponent} from './components/body/body.component';
 import {CartCounterDetailsComponent} from './components/cart-counter-details/cart-counter-details.component';
 import {WishListCounterDetailsComponent} from './components/wish-list-counter-details/wish-list-counter-details.component';
 import {SingleproductComponent} from './components/singleproduct/singleproduct.component';
+import {RegisterformComponent} from './components/registerform/registerform.component';
 const routes: Routes = [
 { path: '', redirectTo: '/home', pathMatch: 'full' },
 {path:'home' ,component:BodyComponent},
 {path:'cartlist' , component:CartCounterDetailsComponent},
 {path:'wishlist' , component:WishListCounterDetailsComponent},
-{path:'productitem' , component:SingleproductComponent}
+{path:'productitem' , component:SingleproductComponent},
+{path: 'registerForm',component:RegisterformComponent}
 ];
 
 @NgModule({
