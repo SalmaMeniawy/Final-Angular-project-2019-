e5d2d45b9a5bfd055a95d5aa5ed1c50c39e418a6
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-08 01:05:29 +0200

    commit for create ts file for login form in login form component

--- app/components/loginform/loginform.component.ts
+++ app/components/loginform/loginform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl ,FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-loginform',
@@ -8,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class LoginformComponent implements OnInit {
 
   constructor() { }
-
+  loginForm = new FormGroup({
+    userName : new FormControl('',[Validators.required]),
+    password : new FormControl('',[Validators.required]),
+  });
   ngOnInit() {
   }
 
