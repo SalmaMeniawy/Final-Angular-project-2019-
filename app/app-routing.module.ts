de7b7d010c0be8132cace5878a4ed07aa4863c7c
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-07 22:30:09 +0200

    commit for add routing for each product can transfer to single product page

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {BodyComponent} from './components/body/body.component';
 import {CartCounterDetailsComponent} from './components/cart-counter-details/cart-counter-details.component';
 import {WishListCounterDetailsComponent} from './components/wish-list-counter-details/wish-list-counter-details.component';
+import {SingleproductComponent} from './components/singleproduct/singleproduct.component';
 const routes: Routes = [
 { path: '', redirectTo: '/home', pathMatch: 'full' },
 {path:'home' ,component:BodyComponent},
 {path:'cartlist' , component:CartCounterDetailsComponent},
-{path:'wishlist' , component:WishListCounterDetailsComponent}
+{path:'wishlist' , component:WishListCounterDetailsComponent},
+{path:'productitem' , component:SingleproductComponent}
 ];
 
 @NgModule({
