7b999db9998f29f3b4bbdd697ce664546308565c
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-09 18:28:33 +0200

    commit for create the content of product deliver service

--- app/services/product-deliver.service.ts
+++ app/services/product-deliver.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, Subject } from 'rxjs';
+import { Product } from '../models/product';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductDeliverService {
+  private subject = new Subject<any>();
+  sendProduct(product: Product) {
+    this.subject.next(product);
+  }
 
+  getProduct(): Observable<any> {
+    return this.subject.asObservable();
+  }
+
+  clearProduct(): void {
+    this.subject.next();
+  }
   constructor() { }
 }
