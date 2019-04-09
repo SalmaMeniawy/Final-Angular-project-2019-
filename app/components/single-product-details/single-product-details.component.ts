0a44d72f6eb03dd1b40d5edac4a190812eeb13a9
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-09 23:02:57 +0200

    commit for create the content of single product details in html file and ts , edit the route of single product component

--- app/components/single-product-details/single-product-details.component.ts
+++ app/components/single-product-details/single-product-details.component.ts
@@ -1,29 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ProductDisplayService } from 'src/app/services/product-display.service';
+// import { Subscription, from } from 'rxjs';
+// import { ProductDisplayService } from 'src/app/services/product-display.service';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { products } from 'src/app/data/mock-products';
+
 @Component({
   selector: 'app-single-product-details',
   templateUrl: './single-product-details.component.html',
   styleUrls: ['./single-product-details.component.scss']
 })
 export class SingleProductDetailsComponent implements OnInit {
-  public product: Product;
-  subscription: Subscription;
-  constructor(private productdisplayservice: ProductDisplayService) {
-    this.productdisplayservice.getProduct().subscribe((product) => {
-      if (product) {
-        this.product = product;
-        console.log(product);
+
+  private products: Product[];
+  private selectedProduct: Product;
+
+  constructor(private productsService: ProductService,
+    private route: ActivatedRoute) {
+    this.products = this.productsService.getProducts();
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.products[i]["id"] === this.route.snapshot.paramMap.get('id')) {
+        this.selectedProduct = this.products[i];
+        console.log("We're good");
+        console.log(this.selectedProduct);
       }
-    });
+    }
   }
 
   ngOnInit() {
   }
-  ngOnDestroy() {
-    
-    this.subscription.unsubscribe();
-  }
+
 
 }
