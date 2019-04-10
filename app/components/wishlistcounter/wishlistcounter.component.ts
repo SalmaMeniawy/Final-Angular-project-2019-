598cdd22841b14ca9e8e11c90a6663d9845b824b
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-10 22:26:42 +0200

    commit for build the content of whish list component in ts files that related to wish list service

--- app/components/wishlistcounter/wishlistcounter.component.ts
+++ app/components/wishlistcounter/wishlistcounter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductWishListService} from 'src/app/services/product-wish-list.service';
+import { Product } from 'src/app/models/product';
+import { products } from 'src/app/data/mock-products';
 
 @Component({
   selector: 'app-wishlistcounter',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wishlistcounter.component.scss']
 })
 export class WishlistcounterComponent implements OnInit {
-
-  constructor() { }
+  private wishListProducts : Product[]=[];
+  private numberOfProducts :number = 0;
+  constructor(private productWishListService:ProductWishListService) { 
+    this.productWishListService.getProduct().subscribe((product )=>{
+      if(product)
+      {
+        if(this.wishListProducts.indexOf(product) === -1){
+          this.wishListProducts.push(product);
+          this.numberOfProducts = this.wishListProducts.length;
+        }
+        else{
+          alert("product is already exist in wish List");
+        }
+      }
+    });
+  }
 
   ngOnInit() {
   }
-
+  removeProductFromList(product)
+  {
+    if(this.wishListProducts.indexOf(product) === 1 )
+    {
+      this.wishListProducts.splice(this.wishListProducts.indexOf(product) , 1);
+    }
+    this.numberOfProducts = this.wishListProducts.length;
+  }
 }
