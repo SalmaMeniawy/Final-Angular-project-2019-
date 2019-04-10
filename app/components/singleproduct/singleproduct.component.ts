51bb5a6c258a6bd2f7a6d596e3e6fb90f57e1eb1
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-04-10 18:51:43 +0200

    commit for use service product wish list service in single product component ts file

--- app/components/singleproduct/singleproduct.component.ts
+++ app/components/singleproduct/singleproduct.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import {ProductDeliverService} from 'src/app/services/product-deliver.service';
-
+import { ProductDeliverService } from 'src/app/services/product-deliver.service';
+import { ProductWishListService } from 'src/app/services/product-wish-list.service';
 @Component({
   selector: 'app-singleproduct',
   templateUrl: './singleproduct.component.html',
@@ -10,12 +10,15 @@ import {ProductDeliverService} from 'src/app/services/product-deliver.service';
 export class SingleproductComponent implements OnInit {
 
   @Input() product: Product;
-  constructor(private productDeliver: ProductDeliverService) { }
+  constructor(private productDeliver: ProductDeliverService,
+    private wishListDeliver: ProductWishListService) { }
 
   ngOnInit() {
   }
-  deliverProduct(){
+  deliverProduct() {
     this.productDeliver.sendProduct(this.product);
   }
-
+  deliverWishList() {
+    this.wishListDeliver.sendProduct(this.product);
+  }
 }
